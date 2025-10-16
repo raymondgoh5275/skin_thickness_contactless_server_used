9e966d04efd99652469222a63dfe2ad059e23d14
KJGoh <KJGoh@ATSSGNB069>
2025-10-16 11:38:47 +0800

    changes for config setting, update webconfig in web services

--- DAL/Properties/Settings.Designer.cs
+++ DAL/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DAL.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace DAL.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ATSUKNB054\\SQLEXPRESS; Initial Catalog=skin_thickness;Integrated Secu" +
-            "rity=True;Asynchronous Processing=True;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ATSSGNB069\\SQLDEVELOPER;Initial Catalog=skin_thickness_contactless; U" +
+            "ser ID=sa; Password=12345; Asynchronous Processing=True;")]
         public string DB_Connection {
             get {
                 return ((string)(this["DB_Connection"]));
